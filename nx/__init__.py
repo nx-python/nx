7ed7f24a0a94108c727a7f6e20ce4d5293cf0ace
Aileen Lumina <aileenlumina@users.noreply.github.com>
2018-03-30 01:37:54 +0200

    Slightly optimized titles

--- nx/__init__.py
+++ nx/__init__.py
@@ -15,9 +15,16 @@ p3 = Player(3)
 p4 = Player(4)
 
 
-class _Titles(dict):
+class _Titles:
+    def __init__(self):
+        self._titles = {}
+
     def __getitem__(self, title_id):
-        return Title(title_id)
+        if title_id in self._titles:
+            return self._titles[title_id]
+        _title = Title(title_id)
+        self._titles[title_id] = _title
+        return _title
 
 
 titles = _Titles()
--- nx/title.py
+++ nx/title.py
@@ -2,7 +2,7 @@ from .filesystem import RomFS, Savedata
 
 
 class Title:
-    def __init__(self, title_id: int):
-        self.title_id = title_id
+    def __init__(self, id: int):
+        self.id = id
         self.romfs = RomFS(self)
         self.savedata = Savedata(self)
